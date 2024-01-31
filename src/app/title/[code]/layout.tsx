d1c66e36441c5f6750af1519a673db9dc126454c
ealks@Admin <ealkser@gmail.com>
2024-02-01 02:26:11 +0900

    change

--- src/app/title/[code]/layout.tsx
+++ src/app/title/[code]/layout.tsx
@@ -1,54 +1,57 @@
-// async function getTitle(code: string) {
-// 	return await getAnilibria
-// 		.title(`?code=${code}`)
-// 		.then(res => res)
-// 		.catch((err: Error) => {
-// 			throw new Error(err.message)
-// 		})
-// }
+import { getAnilibria } from '@/service/anilibria.service'
+import { Metadata } from 'next'
 
-// interface generateMetadataProps {
-// 	params: { code: string }
-// }
+async function getTitle(code: string) {
+	return await getAnilibria
+		.title(`?code=${code}`)
+		.then(res => res)
+		.catch((err: Error) => {
+			throw new Error(err.message)
+		})
+}
 
-// export async function generateMetadata({
-// 	params: { code },
-// }: generateMetadataProps) {
-// 	const data = await getTitle(code)
+interface generateMetadataProps {
+	params: { code: string }
+}
 
-// 	const metadata: Metadata = {
-// 		title: data?.names?.ru ?? code,
-// 		description: String(data.description ?? code),
-// 		openGraph: {
-// 			images: [
-// 				'https://static-libria.weekstorm.one/' + data.posters.small.url,
-// 				'https://static-libria.weekstorm.one/' + data.posters.medium.url,
-// 				'https://static-libria.weekstorm.one/' + data.posters.original.url,
-// 			],
-// 			description: String(data.description ?? code),
-// 			title: data?.names?.ru ?? code,
-// 			type: 'website',
-// 			url: `/title/${code}`,
-// 		},
+export async function generateMetadata({
+	params: { code },
+}: generateMetadataProps) {
+	const data = await getTitle(code)
 
-// 		icons: {
-// 			icon: [
-// 				{
-// 					media: '(prefers-color-scheme: light)',
-// 					url: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
-// 					href: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
-// 				},
-// 				{
-// 					media: '(prefers-color-scheme: dark)',
-// 					url: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
-// 					href: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
-// 				},
-// 			],
-// 		},
-// 	}
+	const metadata: Metadata = {
+		title: data?.names?.ru ?? code,
+		description: String(data.description ?? code),
+		openGraph: {
+			images: [
+				'https://static-libria.weekstorm.one/' + data.posters.small.url,
+				'https://static-libria.weekstorm.one/' + data.posters.medium.url,
+				'https://static-libria.weekstorm.one/' + data.posters.original.url,
+			],
+			description: String(data.description ?? code),
+			title: data?.names?.ru ?? code,
+			type: 'website',
+			url: `/title/${code}`,
+		},
 
-// 	return metadata
-// }
+		icons: {
+			icon: [
+				{
+					media: '(prefers-color-scheme: light)',
+					url: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
+					href: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
+				},
+				{
+					media: '(prefers-color-scheme: dark)',
+					url: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
+					href: 'https://static-libria.weekstorm.one/' + data.posters.small.url,
+				},
+			],
+		},
+	}
+
+	return metadata
+}
 
 interface LayoutProps {
 	children: React.ReactNode
