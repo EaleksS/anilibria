17e484ef05e3b6829489d8d47b7e8ab6f86ceb98
ealks@Admin <ealkser@gmail.com>
2024-02-01 01:33:19 +0900

    change

--- src/service/anilibria.service.ts
+++ src/service/anilibria.service.ts
@@ -9,17 +9,23 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL as string
 export const getAnilibria = {
 	async search(params?: string, sort?: string) {
 		return (await axios
-			.get(`/title/search${!!params ? params : ''}${!!sort ? sort : ''}`)
+			.get(`/title/search${!!params ? params : ''}${!!sort ? sort : ''}`, {
+				headers: { Accept: 'application/json' },
+			})
 			.then(res => res.data)) as TitlesDataT
 	},
 	async title(sort?: string) {
 		return (await axios
-			.get(`/title${!!sort ? sort : ''}`)
+			.get(`/title${!!sort ? sort : ''}`, {
+				headers: { Accept: 'application/json' },
+			})
 			.then(res => res.data)) as TitleT
 	},
 	async random(sort?: string) {
 		return (await axios
-			.get(`/title/random${!!sort ? sort : ''}`)
+			.get(`/title/random${!!sort ? sort : ''}`, {
+				headers: { Accept: 'application/json' },
+			})
 			.then(res => res.data)) as TitleT
 	},
 }
