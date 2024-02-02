26ecb50e625a9d37c65d7a3ffd329e3ae314b748
ealks@Admin <ealkser@gmail.com>
2024-02-03 03:12:11 +0900

    fix

--- src/widgets/ui/header/Dropdown.tsx
+++ src/widgets/ui/header/Dropdown.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useFavoriteQuery } from '@/libs/hook/query/useFavoriteQuery'
-import { useUserQuery } from '@/libs/hook/query/useUserQuery'
 import {
 	Button,
 	Dropdown,
@@ -19,80 +17,81 @@ interface UiDropdownProps {
 }
 
 export const UiDropdown: React.FC<UiDropdownProps> = ({ session }) => {
-	const query = useUserQuery(session)
-	const queryFavorite = useFavoriteQuery(session)
+	// const query = useUserQuery(session)
+	// const queryFavorite = useFavoriteQuery(session)
+	return null
 
-	if (!session)
-		return (
-			<Button
-				variant='flat'
-				className='rounded-full overflow-hidden'
-				as={Link}
-				href='/auth'
-			>
-				Войти
-			</Button>
-		)
+	// if (!session)
+	// 	return (
+	// 		<Button
+	// 			variant='flat'
+	// 			className='rounded-full overflow-hidden'
+	// 			as={Link}
+	// 			href='/auth'
+	// 		>
+	// 			Войти
+	// 		</Button>
+	// 	)
 
-	return (
-		<>
-			<Dropdown>
-				<DropdownTrigger>
-					<Button
-						variant='light'
-						isIconOnly
-						className='rounded-full overflow-hidden'
-					>
-						<Image
-							src={`${process.env.NEXT_PUBLIC_IMG_URL}${
-								query?.data?.avatar_original ?? query?.data?.avatar_thumbnail
-							}`}
-							width='100'
-							height='100'
-							alt='profile'
-						/>
-					</Button>
-				</DropdownTrigger>
-				<DropdownMenu
-					variant='faded'
-					aria-label='Dropdown menu with description'
-					closeOnSelect
-				>
-					<DropdownSection title={query?.data?.email ?? '-'} showDivider>
-						<DropdownItem key='profile' className='h-14 gap-2'>
-							<p className='font-semibold'>Вошёл как</p>
-							<p>{query?.data?.login ?? '-'}</p>
-						</DropdownItem>
-					</DropdownSection>
-					<DropdownSection title='Информация'>
-						<DropdownItem key='chatID' description='В избранных'>
-							<p className='font-semibold'>
-								#{queryFavorite.data?.pagination?.total_items}
-							</p>
-						</DropdownItem>
-					</DropdownSection>
-					<DropdownSection title='Действие'>
-						<DropdownItem
-							key='change-chatId1'
-							className='text-primary'
-							color='primary'
-							href='/favorite'
-						>
-							Перейти в избранные
-						</DropdownItem>
-						<DropdownItem
-							key='change-chatId2'
-							className='text-danger'
-							color='danger'
-							onPress={() => {
-								// localStorage.removeItem('session')
-							}}
-						>
-							Выйти из аккаунта
-						</DropdownItem>
-					</DropdownSection>
-				</DropdownMenu>
-			</Dropdown>
-		</>
-	)
+	// return (
+	// 	<>
+	// 		<Dropdown>
+	// 			<DropdownTrigger>
+	// 				<Button
+	// 					variant='light'
+	// 					isIconOnly
+	// 					className='rounded-full overflow-hidden'
+	// 				>
+	// 					<Image
+	// 						src={`${process.env.NEXT_PUBLIC_IMG_URL}${
+	// 							query?.data?.avatar_original ?? query?.data?.avatar_thumbnail
+	// 						}`}
+	// 						width='100'
+	// 						height='100'
+	// 						alt='profile'
+	// 					/>
+	// 				</Button>
+	// 			</DropdownTrigger>
+	// 			<DropdownMenu
+	// 				variant='faded'
+	// 				aria-label='Dropdown menu with description'
+	// 				closeOnSelect
+	// 			>
+	// 				<DropdownSection title={query?.data?.email ?? '-'} showDivider>
+	// 					<DropdownItem key='profile' className='h-14 gap-2'>
+	// 						<p className='font-semibold'>Вошёл как</p>
+	// 						<p>{query?.data?.login ?? '-'}</p>
+	// 					</DropdownItem>
+	// 				</DropdownSection>
+	// 				<DropdownSection title='Информация'>
+	// 					<DropdownItem key='chatID' description='В избранных'>
+	// 						<p className='font-semibold'>
+	// 							#{queryFavorite.data?.pagination?.total_items}
+	// 						</p>
+	// 					</DropdownItem>
+	// 				</DropdownSection>
+	// 				<DropdownSection title='Действие'>
+	// 					<DropdownItem
+	// 						key='change-chatId1'
+	// 						className='text-primary'
+	// 						color='primary'
+	// 						href='/favorite'
+	// 					>
+	// 						Перейти в избранные
+	// 					</DropdownItem>
+	// 					<DropdownItem
+	// 						key='change-chatId2'
+	// 						className='text-danger'
+	// 						color='danger'
+	// 						onPress={() => {
+	// 							// localStorage.removeItem('session')
+	// 						}}
+	// 					>
+	// 						Выйти из аккаунта
+	// 					</DropdownItem>
+	// 				</DropdownSection>
+	// 			</DropdownMenu>
+	// 		</Dropdown>
+	// 	</>
+	// )
 }
