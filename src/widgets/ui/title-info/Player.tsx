00e32fe982a746bc10a310ee5acf4935916a590c
ealks@Admin <ealkser@gmail.com>
2024-02-04 18:19:10 +0900

    fix

--- src/widgets/ui/title-info/Player.tsx
+++ src/widgets/ui/title-info/Player.tsx
@@ -41,12 +41,11 @@ export const Player: React.FC<PlayerProps> = ({ ...props }) => {
 	})
 	if (props.isLoading) return <Spinner />
 
-	const youtubeShikamori = React.useMemo(
-		() => props?.shikamory?.filter(e => e.hosting === 'youtube'),
-		[props?.shikamory?.length]
+	const youtubeShikamori = props?.shikamory?.filter(
+		e => e.hosting === 'youtube'
 	)
 
-	if (!props.player?.episodes.last) return 'К сожалению серий нету'
+	// if (!props.player?.episodes.last) return 'К сожалению серий нету'
 
 	return (
 		<>
@@ -58,85 +57,91 @@ export const Player: React.FC<PlayerProps> = ({ ...props }) => {
 				onSelectionChange={setSelected}
 			>
 				<Tab key='me' title='Наш плеер' className='w-full flex flex-col gap-3'>
-					<div className='flex gap-3 flex-wrap md:flex-nowrap w-full lg:w-[500px]'>
-						<Select
-							label='Серия'
-							variant='bordered'
-							selectedKeys={value}
-							onSelectionChange={e => {
-								if (typeof Array.from(e)[0] === 'undefined') return
-								setEpisodes(Array.from(e)[0] ?? '1')
-								setValue(e)
-							}}
-							required
-							isRequired
-							size='sm'
-						>
-							{episodesData.map(episode => (
-								<SelectItem key={episode.value} value={episode.value}>
-									{episode.label}
-								</SelectItem>
-							))}
-						</Select>
-						<Select
-							label='Качество видео'
-							variant='bordered'
-							selectedKeys={valuehls}
-							className='w-[200px]'
-							size='sm'
-							onSelectionChange={e => {
-								if (typeof Array.from(e)[0] === 'undefined') return
+					{!props.player?.episodes.last ? (
+						'К сожалению серий нету'
+					) : (
+						<>
+							<div className='flex gap-3 flex-wrap md:flex-nowrap w-full lg:w-[500px]'>
+								<Select
+									label='Серия'
+									variant='bordered'
+									selectedKeys={value}
+									onSelectionChange={e => {
+										if (typeof Array.from(e)[0] === 'undefined') return
+										setEpisodes(Array.from(e)[0] ?? '1')
+										setValue(e)
+									}}
+									required
+									isRequired
+									size='sm'
+								>
+									{episodesData.map(episode => (
+										<SelectItem key={episode.value} value={episode.value}>
+											{episode.label}
+										</SelectItem>
+									))}
+								</Select>
+								<Select
+									label='Качество видео'
+									variant='bordered'
+									selectedKeys={valuehls}
+									className='w-[200px]'
+									size='sm'
+									onSelectionChange={e => {
+										if (typeof Array.from(e)[0] === 'undefined') return
 
-								const hlsD =
-									Array.from(e)[0] === 'hd'
-										? 'hd'
-										: Array.from(e)[0] === 'sd'
-										? 'sd'
-										: Array.from(e)[0] === 'fhd'
-										? 'fhd'
-										: 'hd'
+										const hlsD =
+											Array.from(e)[0] === 'hd'
+												? 'hd'
+												: Array.from(e)[0] === 'sd'
+												? 'sd'
+												: Array.from(e)[0] === 'fhd'
+												? 'fhd'
+												: 'hd'
 
-								setHls(hlsD)
-								setValuehls(e)
-							}}
-							required
-							isRequired
-						>
-							{hlsData.map(episode => (
-								<SelectItem key={episode.value} value={episode.value}>
-									{episode.label}
-								</SelectItem>
-							))}
-						</Select>
-					</div>
-					<FilePlayer
-						className='aspect-video w-full rounded-large object-cover overflow-hidden'
-						width='854'
-						height='480'
-						controls={true}
-						fallback={
-							<Spinner className=' absolute top-0 bottom-0 right-0 left-0 flex items-center justify-center' />
-						}
-						onPause={() => {}}
-						onPlay={() => {}}
-						light={`${process.env.NEXT_PUBLIC_IMG_URL}/${
-							props?.player?.list[episodes ?? '1'].preview ?? ''
-						}`}
-						url={`https://${props?.player?.host}${
-							props?.player?.list[episodes ?? '1']?.hls?.[hls ?? 'sd']
-						}`}
-					/>
+										setHls(hlsD)
+										setValuehls(e)
+									}}
+									required
+									isRequired
+								>
+									{hlsData.map(episode => (
+										<SelectItem key={episode.value} value={episode.value}>
+											{episode.label}
+										</SelectItem>
+									))}
+								</Select>
+							</div>
+							<FilePlayer
+								className='aspect-video w-full rounded-large object-cover overflow-hidden'
+								width='854'
+								height='480'
+								controls={true}
+								fallback={
+									<Spinner className=' absolute top-0 bottom-0 right-0 left-0 flex items-center justify-center' />
+								}
+								onPause={() => {}}
+								onPlay={() => {}}
+								light={`${process.env.NEXT_PUBLIC_IMG_URL}/${
+									props?.player?.list[episodes ?? '1'].preview ?? ''
+								}`}
+								url={`https://${props?.player?.host}${
+									props?.player?.list[episodes ?? '1']?.hls?.[hls ?? 'sd']
+								}`}
+							/>
+						</>
+					)}
 				</Tab>
 				<Tab
 					key='alter'
 					title='Сторонний плеер'
 					className='w-full aspect-video'
 				>
-					{props.player.alternative_player ? (
+					{props?.player?.alternative_player ? (
 						<iframe
 							width='100%'
 							height='100%'
-							src={props.player.alternative_player}
+							src={props?.player?.alternative_player}
 							className='aspect-video w-full object-cover rounded-large'
 						></iframe>
 					) : (
